f12391ef5e148951654e58c17a3dba3cdedb32be
sbobovyc <sbobovyc@scorpia.(none)>
2011-12-04 04:11:35 -0600

    Started working on mapmaker

---
+++ src/utils/mapmaker/src/libmmaker.hpp
@@ -0,0 +1,9 @@
+#ifndef LIBMMAKER_H
+#define LIBMMAKER_H
+
+#define EARTH_WIDTH 12756.2 /// in km
+#define EARTH_HEITH 12713.6 /// in km
+
+extern "C" void render_thumb(int);
+
+#endif /* LIBMMAKER_H */
---
+++ src/utils/mapmaker/src/mapmaker.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+#include "libmmaker.hpp"
+
+int main(int argc, char ** argv) {
+	render_thumb(200);
+	return 0;
+}
