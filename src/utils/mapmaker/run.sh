5e8df9b25d1cc49bb586abfd57518f3d58281e44
Stan Bobovych <stan.bobovych@gmail.com>
2011-12-11 02:21:35 -0600

    Determined method for continent border detection using computer vision techniques. Determined that the cylindrical projection is unsuitable for creating vertically seamless terrain. Need to switch to spherical or planar projection.

--- src/utils/mapmaker/run.sh
+++ src/utils/mapmaker/run.sh
@@ -1,5 +1,8 @@
 export LD_LIBRARY_PATH=.
-./mapmaker
-#gdal_translate -of GTiff -a_srs "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs" -a_ullr -180 90 180 -90 tutorial.bmp tutorial.tif
+#./mapmaker
+convert world_one_map.bmp -edge 1 -colorspace Gray world_one_map_mask.png
+convert world_one_map_mask.png -blur 0x2.0 world_one_map_mask_blur.png
+convert world_one_map_mask_blur.png -threshold 10% world_one_map_thresh.pgm
 
-
+gdal_translate -of GTiff -a_srs "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs" -a_ullr -180 90 180 -90 world_one_map.bmp world_one_map.tif
+gdal_translate -of GTiff -a_srs "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs" -a_ullr -180 90 180 -90 world_one_map_thresh.pgm world_one_map_thresh.tif
