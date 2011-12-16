6847deb8cc589349ac4b923e39e1809fc6e32ed9
Stan Bobovych <stan.bobovych@gmail.com>
2011-12-15 18:25:42 -0600

    Experimentation with edge detection has led to improved world map border detection.

--- src/utils/mapmaker/run.sh
+++ src/utils/mapmaker/run.sh
@@ -1,9 +1,17 @@
 export LD_LIBRARY_PATH=.
 ./mapmaker
-convert world_one_map.bmp -edge 1 -colorspace Gray world_one_map_mask.png
-convert world_one_map_mask.png -blur 0x2.0 world_one_map_mask_blur.png
-convert world_one_map_mask_blur.png -threshold 10% world_one_map_thresh.pgm
+#convert world_one_map.bmp -negate -morphology EdgeOut Diamond -colorspace Gray world_one_edge.png
+#convert world_one_edge.png -blur 0x2.0 world_one_edge_blur.png
+#convert world_one_edge_blur.png -threshold 5% world_one_thresh.png
+#convert world_one_thresh.png -negate -morphology Edge Diamond -colorspace Gray world_one_borders.png
 
-gdal_translate -of GTiff -a_srs "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs" -a_ullr -180 90 180 -90 world_one_map.bmp world_one_map.tif
-gdal_translate -of GTiff -a_srs "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs" -a_ullr -180 90 180 -90 world_one_map_thresh.pgm world_one_map_thresh.tif
-gdal_polygonize.py world_one_map_thresh.tif -f "ESRI Shapefile" border.shp
+convert world_one_map.bmp -negate -morphology EdgeOut Diamond -colorspace Gray world_one_edge.png
+convert world_one_edge.png -threshold 10% world_one_thresh.png
+convert world_one_thresh.png -morphology Close Disk:2 world_one_close.png
+convert world_one_close.png -morphology EdgeIn Plus world_one_borders.png
+
+#gdal_translate -of GTiff -a_srs "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs" -a_ullr -180 90 180 -90 world_one_map.bmp world_one_map.tif
+#gdal_translate -of GTiff -a_srs "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs" -a_ullr -180 90 180 -90 world_one_borders.png world_one_borders.tif
+#gdal_polygonize.py world_one_borders.tif -f "ESRI Shapefile" world_one_borders.shp
+#ogr2ogr -f SQLite world_one_borders.sqlite world_one_borders.shp -dsco spatialite=yes
+#
--- src/utils/mapmaker/src/mapmaker.cpp
+++ src/utils/mapmaker/src/mapmaker.cpp
@@ -5,6 +5,6 @@
 int main(int argc, char ** argv) {
 	char name[] = "world_one_map";
 	render_thumb(200, name);
-	render_tiles(200, 1200, 350, 1, 1, 30, 30, 512, 256, name);
+	//render_tiles(200, 1200, 350, 1, 1, 30, 30, 512, 256, name);
 	return 0;
 }
