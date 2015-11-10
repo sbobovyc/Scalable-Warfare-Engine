3a3a37e55c661add567f3c9008628e29c76d1a23
Stan Bobovych <stan.bobovych@gmail.com>
2015-11-10 12:29:39 -0500

    * Added much better Syrian province borders * Added Turkey border

--- src/web/map.js
+++ src/web/map.js
@@ -1,4 +1,5 @@
 // http://christopherjennison.com/openlayers3-quickstart-tutorial/
+// http://openlayers.org/en/v3.6.0/examples/vector-layer.html
 console.log('SWE: starting render');
 console.time('render')
 var start = +new Date();  // log start timestamp
@@ -16,11 +17,45 @@ var map = new ol.Map({
     //          url: './content/110m_land.geojson', 
     //          format: new ol.format.GeoJSON()})
     //}),
+    //new ol.layer.Vector({
+    //  title: 'vector_layer_borders',
+    //  source: new ol.source.Vector({
+    //          projection: 'EPGS:4326',
+    //          url: './content/TM_WORLD_BORDERS_SIMPL-0.3.geojson', 
+    //          format: new ol.format.GeoJSON()})
+    //}),
     new ol.layer.Vector({
-      title: 'vector_layer_borders',
+      title: 'vector_layer_turkey0',
       source: new ol.source.Vector({
               projection: 'EPGS:4326',
-              url: './content/TM_WORLD_BORDERS_SIMPL-0.3.geojson', 
+              url: './content/TUR_adm0.geojson', 
+              format: new ol.format.GeoJSON()
+            }),
+      style: new ol.style.Style({
+                stroke: new ol.style.Stroke({
+                color: '#ccb',
+                width: 2})
+                })
+    }),
+    new ol.layer.Vector({
+      title: 'vector_layer_syria0',
+      source: new ol.source.Vector({
+              projection: 'EPGS:4326',
+              url: './content/SYR_adm0.geojson', 
+              format: new ol.format.GeoJSON()})
+    }),
+    new ol.layer.Vector({
+      title: 'vector_layer_syria1',
+      source: new ol.source.Vector({
+              projection: 'EPGS:4326',
+              url: './content/SYR_adm1.geojson', 
+              format: new ol.format.GeoJSON()})
+    }),
+    new ol.layer.Vector({
+      title: 'vector_layer_syria2',
+      source: new ol.source.Vector({
+              projection: 'EPGS:4326',
+              url: './content/SYR_adm2.geojson', 
               format: new ol.format.GeoJSON()})
     })
   ],
